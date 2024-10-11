0e711bae479d05602e8b2f68db03aca99f8c8621
MinkeySon <126847381+MinkeySon@users.noreply.github.com>
2024-10-11 20:36:28 +0900

    :beers: Update java 2839

--- src/main/java/org/algorithm/sovle/class2/BJ2839_설탕배달.java
+++ src/main/java/org/algorithm/sovle/class2/BJ2839_설탕배달.java
@@ -4,6 +4,10 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+
+/**
+ * 그리디 알고리즘
+ */
 public class BJ2839_설탕배달 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -12,19 +16,22 @@ public class BJ2839_설탕배달 {
 
         // 0이 될때까지 반복
         while(N != 0){
+            // N이 5와 3의 공배수가 아닐경우
             if(N % 3 == 0 && N % 5 == 0){
                 cnt++;
                 N -= 5;
             }
+            // 3의 배수가 아닐경우
             else if(N % 3 == 0){
                 cnt ++;
                 N -= 3;
+                // 5의 배수가 아닐 경우
             } else if (N % 5 == 0) {
                 cnt ++;
                 N -= 5;
             }else{
                 N -= 5; // 둘 다 안나눠 진다? ex) 11. 일단 5를 빼
-                if(N<0){
+                if(N<0){ // 딱 맞게 나눌 수가 없는 수
                     cnt = -1;
                     break;
                 }else{
@@ -32,6 +39,6 @@ public class BJ2839_설탕배달 {
                 }
             }
         }
-        System.out.println(cnt);
+        System.out.println(cnt); // 최종 출력
     }
 }
