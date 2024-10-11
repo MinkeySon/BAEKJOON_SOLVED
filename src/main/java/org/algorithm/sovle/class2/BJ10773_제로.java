22e156e726107bc9e68d7d25806fbf2bf10bfe69
MinkeySon <126847381+MinkeySon@users.noreply.github.com>
2024-10-11 20:36:00 +0900

    :beers: Update java 10773

---
+++ src/main/java/org/algorithm/sovle/class2/BJ10773_제로.java
@@ -0,0 +1,28 @@
+package org.algorithm.sovle.class2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class BJ10773_제로 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int sum = 0;
+
+        int K = Integer.parseInt(br.readLine());
+        Stack<Integer> s = new Stack<>();
+        for(int i=0; i<K; i++){
+            int N = Integer.parseInt(br.readLine());
+
+            if(N == 0){
+                sum = sum - s.pop();
+            }else{
+                s.push(N);
+                sum = sum + N;
+            }
+        }
+
+        System.out.println(sum);
+    }
+}
