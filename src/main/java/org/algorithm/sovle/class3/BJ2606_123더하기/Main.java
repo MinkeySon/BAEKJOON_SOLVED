bcc9a489a04489007a086ba615d1841e253201be
MinkeySon <126847381+MinkeySon@users.noreply.github.com>
2024-11-05 21:42:55 +0900

    :beers: Update java 9095

---
+++ src/main/java/org/algorithm/sovle/class3/BJ2606_123더하기/Main.java
@@ -0,0 +1,32 @@
+package org.algorithm.sovle.class3.BJ2606_123더하기;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    static int N;
+    static long[] dp = new long[11];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 4;
+
+        for(int i=4; i<=10; i++){
+            for(int j=i-1; j>=i-3; j--){
+                dp[i] += dp[j];
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<N; i++){
+            int input = Integer.parseInt(br.readLine());
+
+            sb.append(dp[input]).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
