52f5f5ebd4ef06d7e58f8c7e4c5ad0dc85547c00
MinkeySon <126847381+MinkeySon@users.noreply.github.com>
2024-11-14 21:44:31 +0900

    :beers: Update java 11279

---
+++ src/main/java/org/algorithm/sovle/class3/BJ11279_최대힙/Main.java
@@ -0,0 +1,33 @@
+package org.algorithm.sovle.class3.BJ11279_최대힙;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class Main {
+    static PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+    static int N;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+
+        for(int i=0; i<N; i++){
+            String input = br.readLine();
+
+            if(input.equals("0")){
+                if(maxHeap.isEmpty()){
+                    sb.append(0).append("\n");
+                }else{
+                    sb.append(maxHeap.poll()).append("\n");
+                }
+            }else{
+                maxHeap.add(Integer.parseInt(input));
+            }
+        }
+        System.out.println(sb);
+
+    }
+}
